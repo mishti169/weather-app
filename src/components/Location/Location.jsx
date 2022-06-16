e5389f15cd9d11f0a1e45767e5fcd25f5ed47873
MANISHA <manishapanchal169@gmail.com>
2022-06-16 16:55:14 +0530

    add location header

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import ChevronLeftLogo from '../../assets/icons/chevron-left.svg';
+import PlusLogo from '../../assets/icons/plus.svg';
+import './Location.css';
+
 const Location = () => {
 	return (
-		<>
-			<div>this is location</div>
-		</>
+		<div className='location'>
+			<div className='location--header'>
+				<div className='location--header_text-icon'>
+					<img src={ChevronLeftLogo} width={21} className='chevron-left' />
+					<span>Select City</span>
+				</div>
+				<img src={PlusLogo} width={21} className='location--header_plus-icon' />
+			</div>
+			<div></div>
+		</div>
 	);
 };
 
