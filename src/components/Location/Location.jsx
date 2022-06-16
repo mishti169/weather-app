56c731d0c2af63141a7002dd552535dbf2a433c8
MANISHA <manishapanchal169@gmail.com>
2022-06-16 17:25:16 +0530

    add location body with styling

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChevronLeftLogo from '../../assets/icons/chevron-left.svg';
 import PlusLogo from '../../assets/icons/plus.svg';
+import CloudRainLogo from '../../assets/icons/cloud-rain.svg';
 import './Location.css';
 
 const Location = () => {
@@ -13,7 +14,14 @@ const Location = () => {
 				</div>
 				<img src={PlusLogo} width={21} className='location--header_plus-icon' />
 			</div>
-			<div></div>
+			<div className='location--body'>
+				<div className='location--body--details '>
+					<span className='location--body--details_city'>Mumbai</span>
+					<span className='location--body--details_temp'>22°C</span>
+					<span className='location--body--details_temp-text'>Light Drizzle</span>
+				</div>
+				<img width={40} src={CloudRainLogo} alt='raining' srcset='' />
+			</div>
 		</div>
 	);
 };
