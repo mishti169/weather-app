9049815659e53991e7456cda47132b52498cfa88
MANISHA <manishapanchal169@gmail.com>
2022-06-16 15:52:46 +0530

    auto redirect

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Splash.css';
 
 const Splash = () => {
 	const history = useHistory();
-	const handleClick = () => {
-		history.push('/home');
-	};
+	useEffect(() => {
+		setTimeout(() => {
+			history.push('/home');
+		}, 1000);
+	}, []);
 	return (
-		<div onClick={handleClick} className='weather-container'>
+		<div className='weather-container'>
 			<div className='weather-text'>Weather</div>
 			<div className='weather-footer'>A minimal weather app</div>
 		</div>
