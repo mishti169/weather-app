cf50f4afec05eaac8a73327b06de5b736507a5ae
MANISHA <manishapanchal169@gmail.com>
2022-06-17 15:25:16 +0530

    import forecast comp. and create route for forecast comp.

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import Home from './components/Home/Home';
 import Location from './components/Location/Location';
 import Settings from './components/Settings/Settings';
 import Details from './components/Details/Details';
+import ForeCast from './components/ForeCast/ForeCast';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Switch>
+				<Route path='/forecast' component={ForeCast} />
 				<Route path='/settings' component={Settings} />
 				<Route path='/location' component={Location} />
 				<Route path='/details' component={Details} />
