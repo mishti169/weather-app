669ab615c57ddc072511bfc341af2e1ee16e40ec
MANISHA <manishapanchal169@gmail.com>
2022-06-17 12:35:36 +0530

    make details body with styling

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from '../Header/Header';
+import TempDetails from '../TempDetails/TempDetails';
 import './Details.css';
+
 const Details = () => {
-	return <div className='border'> this is Details</div>;
+	return (
+		<div className='details'>
+			<Header />
+			<div className='details--body'>
+				<span className='details--body_text'>Details</span>
+				<div className='details--body_inner-body'>
+					<TempDetails />
+					<TempDetails />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Details;
