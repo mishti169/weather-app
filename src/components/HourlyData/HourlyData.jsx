77285ebba85b978cbced0e4f418157f9933b4dc8
MANISHA <manishapanchal169@gmail.com>
2022-06-17 16:37:39 +0530

    make HourlyData comp with styling

--- src/components/HourlyData/HourlyData.jsx
+++ src/components/HourlyData/HourlyData.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import CloudRainLogo from '../../assets/icons/cloud-rain.svg';
 import './HourlyData.css';
 const HourlyData = () => {
-	return <div>this is hourly data </div>;
+	return (
+		<div className='forecast--body--hourly_time-icon '>
+			<div className='forecast--body--hourly_time-icon--container '>
+				<span className='forecast--body--hourly_time'>10:00</span>
+				<img src={CloudRainLogo} width={24} />
+			</div>
+		</div>
+	);
 };
 export default HourlyData;
