c8c6c38ab9d1a9246b8bdcda862bacfb6eef96d3
MANISHA <manishapanchal169@gmail.com>
2022-06-17 12:41:33 +0530

    feat: on chevron click go to /home

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,14 +1,19 @@
+import { useHistory } from 'react-router-dom';
 import ChevronLeftLogo from '../../assets/icons/chevron-left.svg';
 import PlusLogo from '../../assets/icons/plus.svg';
 import CityTemperature from '../CityTemperature/CityTemperature';
 import './Location.css';
 
 const Location = () => {
+	const history = useHistory();
+	const handleClick = () => {
+		history.push('/home');
+	};
 	return (
 		<div className='location'>
 			<div className='location--header'>
 				<div className='location--header_text-icon'>
-					<img src={ChevronLeftLogo} width={21} className='chevron-left' />
+					<img onClick={handleClick} src={ChevronLeftLogo} width={21} className='chevron-left' />
 					<span>Select City</span>
 				</div>
 				<img src={PlusLogo} width={21} className='location--header_plus-icon' />
