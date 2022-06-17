b92828d6d55ff7f2b1db99f7c4f82bc9b02e71d6
MANISHA <manishapanchal169@gmail.com>
2022-06-17 16:24:51 +0530

    add hourly forecast

--- src/components/ForeCast/ForeCast.jsx
+++ src/components/ForeCast/ForeCast.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from '../Header/Header';
 import './ForeCast.css';
+import CloudRainLogo from '../../assets/icons/cloud-rain.svg';
 
 const ForeCast = () => {
 	return (
-		<div>
+		<div className='forecast'>
 			<Header />
+			<div className='forecast--body'>
+				<span className='forecast--body-heading'>Forecast</span>
+				<div className='forecast--body--hourly'>
+					<span className='forecast--body--hourly_title'>Hourly Forecast</span>
+					<div className='forecast--body--hourly_time-icon '>
+						<div className='forecast--body--hourly_time-icon--container '>
+							<span className='forecast--body--hourly_time'>10:00</span>
+							<img src={CloudRainLogo} width={24} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
