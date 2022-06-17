02b9d4e0b8df6b65a5c194fa9b0962da91ac9fa7
MANISHA <manishapanchal169@gmail.com>
2022-06-17 16:37:10 +0530

    refactor to hourlyData

--- src/components/Hourly/Hourly.jsx
+++ src/components/Hourly/Hourly.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
+import HourlyData from '../HourlyData/HourlyData';
 import './Hourly.css';
 
 const Hourly = () => {
 	return (
 		<div className='forecast--body--hourly'>
 			<span className='forecast--body--hourly_title'>Hourly Forecast</span>
-			<div className='forecast--body--hourly_time-icon '>
-				<div className='forecast--body--hourly_time-icon--container '>
-					<span className='forecast--body--hourly_time'>10:00</span>
-					<img src={CloudRainLogo} width={24} />
-				</div>
-			</div>
+			<HourlyData />
 		</div>
 	);
 };
