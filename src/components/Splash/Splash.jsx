a8090c6a06ee5d5ef16f50a0fbdd4902effeda0d
MANISHA <manishapanchal169@gmail.com>
2022-06-18 18:09:18 +0530

    call weather api on splash

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,14 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Splash.css';
+import axios from 'axios';
 
 const Splash = () => {
+	const [location, setLocation] = useState(null);
 	const history = useHistory();
 	useEffect(() => {
-		setTimeout(() => {
-			history.push('/home');
-		}, 1000);
+		getLocation();
 	}, []);
+
+	useEffect(() => {
+		if (location) {
+			axios.get(`https://api.weatherapi.com/v1/forecast.json?key=eacf688b839549519dc105316221806&q=${location.lat},${location.lng}&days=10&aqi=no&alerts=no
+	`);
+		}
+	}, [location]);
+
+	const getLocation = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			return null;
+		}
+	};
+	const showPosition = (position) => {
+		setLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+	};
 	return (
 		<div className='weather-container'>
 			<div className='weather-text'>Weather</div>
