617746f6202d5dd0e8ac9f4fac4023f1c9b9e17d
MANISHA <manishapanchal169@gmail.com>
2022-06-18 11:16:32 +0530

    add settings theme and feedback  with styling

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -5,13 +5,32 @@ import ChevronLeftLogo from '../../assets/icons/chevron-left.svg';
 const Settings = () => {
 	return (
 		<div className='settings'>
-			<div className='setting--header'>
+			<div className='settings--header'>
 				<img width={21} src={ChevronLeftLogo} alt='left-chevron' />
 				<span className='setting--header_title'>Settings</span>
 			</div>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div className='settings--headings'>
+				<span className='settings-titles'>Theme</span>
+				<div className='settings--titles--body'>
+					<span className='settings--titles_sub-titles'>Dark Theme</span>
+					<span className='settings--titles_sub-titles_text'>Join the Dark Side!</span>
+				</div>
+				<div className='settings--titles--body'>
+					<span className='settings--titles_sub-titles'>Light Theme</span>
+					<span className='settings--titles_sub-titles_text'>Let There be Light!</span>
+				</div>
+			</div>
+			<div className='settings--headings'>
+				<span className='settings-titles'>Feedback</span>
+				<div className='settings--titles--body'>
+					<span className='settings--titles_sub-titles'>Report an Issue</span>
+					<span className='settings--titles_sub-titles_text'>Facing an issue? Report and we’ll look into it.</span>
+				</div>
+				<div className='settings--titles--body'>
+					<span className='settings--titles_sub-titles'>Rate on App Store</span>
+					<span className='settings--titles_sub-titles_text'>Enjoying the app? Leave a review on the App Store.</span>
+				</div>
+			</div>
 		</div>
 	);
 };
