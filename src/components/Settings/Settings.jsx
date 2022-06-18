88efd940d562d306945c387a462be40788d440ec
MANISHA <manishapanchal169@gmail.com>
2022-06-18 10:50:18 +0530

    add settings header with styling

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Settings.css';
+import ChevronLeftLogo from '../../assets/icons/chevron-left.svg';
+
 const Settings = () => {
 	return (
-		<>
-			<div>this is settings</div>
-		</>
+		<div className='settings'>
+			<div className='setting--header'>
+				<img width={21} src={ChevronLeftLogo} alt='left-chevron' />
+				<span className='setting--header_title'>Settings</span>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
 	);
 };
 
