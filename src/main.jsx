2633a7f75af8d352d68c8fb4160b7a166417d6a5
MANISHA <manishapanchal169@gmail.com>
2022-06-19 12:16:12 +0530

    remove unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './App';
 import Splash from './components/Splash/Splash';
 import Home from './components/Home/Home';
 import Location from './components/Location/Location';
