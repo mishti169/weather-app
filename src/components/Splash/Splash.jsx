86a133fe71bd57fe6596bc0c891931d65d8b3363
MANISHA <manishapanchal169@gmail.com>
2022-06-19 14:51:01 +0530

    refactor: remove API call and treat as a loader screen

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,47 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import './Splash.css';
-import axios from 'axios';
-import { weather } from '../../globalAtom';
-import { useAtom } from 'jotai';
 
 const Splash = () => {
-	const [location, setLocation] = useState(null);
-	const [, setWeatherData] = useAtom(weather);
-	const history = useHistory();
-
-	const fetchWeather = async () => {
-		const { data } = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${
-			import.meta.env.VITE_API_KEY
-		}&q=${location.lat},${location.lng}&days=10&aqi=no&alerts=no
-	`);
-		return data;
-	};
-
-	useEffect(() => {
-		getLocation();
-	}, []);
-
-	useEffect(() => {
-		if (location) {
-			const data = fetchWeather();
-			console.log(data);
-			setWeatherData(data);
-
-			history.push('/home');
-		}
-	}, [location]);
-
-	const getLocation = () => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(showPosition);
-		} else {
-			return null;
-		}
-	};
-	const showPosition = (position) => {
-		setLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
-	};
 	return (
 		<div className='weather-container'>
 			<div className='weather-text'>Weather</div>
