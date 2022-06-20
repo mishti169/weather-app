312888bd389dcccaaae9360ddba4e0f5ba38ee0d
MANISHA <manishapanchal169@gmail.com>
2022-06-20 12:25:05 +0530

    remove static data with dynamic data in Daily and DailyData page

--- src/components/Daily/Daily.jsx
+++ src/components/Daily/Daily.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import DailyData from '../DailyData/DailyData';
+import { useWeather } from '../../hooks/useWeather';
 
 import './Daily.css';
 
 const Daily = () => {
+	const { data: weatherData } = useWeather();
+	const { forecast } = weatherData;
+	const { forecastday } = forecast;
+
 	return (
 		<div className='forecast--body--daily'>
 			<span className='forecast--body--daily_title'>Daily Forecast</span>
 			<div className='forecast--body--daily_time-icon'>
-				<DailyData />
+				{forecastday.map((currentVal) => {
+					return (
+						<DailyData
+							key={currentVal.date_epoch}
+							date={currentVal.date}
+							maxTemp={currentVal.day.maxtemp_c}
+							minTemp={currentVal.day.mintemp_c}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
--- src/components/DailyData/DailyData.jsx
+++ src/components/DailyData/DailyData.jsx
@@ -4,18 +4,19 @@ import ArrowUpLogo from '../../assets/icons/arrow-up.svg';
 import ArrowDownLogo from '../../assets/icons/arrow-down.svg';
 import './DailyData.css';
 
-const DailyData = () => {
+const DailyData = (props) => {
+	const { date, maxTemp, minTemp } = props;
 	return (
 		<div className='forecast--body--daily_time-icon-temp--container '>
-			<span className='forecast--body--daily_time-date'>26 Dec</span>
+			<span className='forecast--body--daily_time-date'>{date}</span>
 			<img src={CloudRainLogo} width={24} />
 			<div className='home--body_temp_summary--detail'>
 				<img width={11} src={ArrowUpLogo} />
-				<span className='forecast--body--daily_time-temp'>26℃</span>
+				<span className='forecast--body--daily_time-temp'>{maxTemp}℃</span>
 			</div>
 			<div className='home--body_temp_summary--detail'>
 				<img width={11} src={ArrowDownLogo} />
-				<span className='forecast--body--daily_time-temp'>16℃</span>
+				<span className='forecast--body--daily_time-temp'>{minTemp}℃</span>
 			</div>
 		</div>
 	);
