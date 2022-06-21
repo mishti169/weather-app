5f0a31241c622e8062e3dd309e4da0698cb32d10
MANISHA <manishapanchal169@gmail.com>
2022-06-21 19:11:53 +0530

    add location support in CTemp

--- src/components/CityTemperature/CityTemperature.jsx
+++ src/components/CityTemperature/CityTemperature.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import CloudRainLogo from '../../assets/icons/cloud-rain.svg';
+import { useWeather } from '../../hooks/useWeather';
+import { getWeatherIcon } from '../../util/utils';
 import './CityTemperature.css';
 
-const CityTemperature = () => {
+const CityTemperature = (props) => {
+	const { location } = props;
+	const { loading, data: weatherData } = useWeather(location);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	const getIcon = (code) => {
+		const Icon = getWeatherIcon(code);
+		return <Icon width={40} />;
+	};
+
 	return (
 		<div className='city-temp'>
 			<div className='city-temp--details '>
-				<span className='city-temp--details_city'>Mumbai</span>
-				<span className='city-temp--details_temp'>22°C</span>
-				<span className='city-temp--details_temp-text'>Light Drizzle</span>
+				<span className='city-temp--details_city'>{weatherData?.location?.name}</span>
+				<span className='city-temp--details_temp'>{weatherData?.current?.temp_c}°C</span>
+				<span className='city-temp--details_temp-text'>{weatherData?.current?.condition?.text}</span>
 			</div>
-			<img width={40} src={CloudRainLogo} alt='raining' />
+			{getIcon(weatherData.current.condition.code)}
 		</div>
 	);
 };
